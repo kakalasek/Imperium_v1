6ccce8d718295b15b8e636f9435d470690f73e67
Kakalasek <honziccekkj@seznam.cz>
2024-07-15 19:29:06 +0200

    basic routes

--- controller/main.py
+++ controller/main.py
@@ -6,5 +6,21 @@ app = Flask(__name__)
 def home():
     return render_template('home.html', title='Home')
 
+@app.route("/scanner")
+def scanner():
+    return render_template('scanner.html', title='Scanner')
+
+@app.route("/diagnostics")
+def diagnostics():
+    return render_template('diagnostics.html', title='Diagnostics')
+
+@app.route("/password_cracker")
+def password_cracker():
+    return render_template('password_cracker.html', title='Password Cracker')
+
+@app.route("/social_engineering")
+def social_engineering():
+    return render_template('social_engineering.html', title='Social Engineering')
+
 if __name__ == '__main__':
     app.run(debug=True)
