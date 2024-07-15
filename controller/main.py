7baef0e9eee16b2acbc75d956141a8517848ef9d
Kakalasek <honziccekkj@seznam.cz>
2024-07-15 17:17:45 +0200

    navbar, sidebar, default templates

--- controller/main.py
+++ controller/main.py
@@ -2,8 +2,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+@app.route("/home")
 def home():
-    return "Hello there"
+    return render_template('home.html', title='Home')
 
 if __name__ == '__main__':
     app.run(debug=True)
