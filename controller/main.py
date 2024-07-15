1170d3745df56848650c55eef2f11a17a1c15c44
Kakalasek <honziccekkj@seznam.cz>
2024-07-15 20:28:34 +0200

    forms

--- controller/main.py
+++ controller/main.py
@@ -1,5 +1,8 @@
 from flask import Flask, render_template
+from forms import ApiForm
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = "73eeac3fa1a0ce48f381ca1e6d71f077"
 
 @app.route("/")
 @app.route("/home")
@@ -8,19 +11,23 @@ def home():
 
 @app.route("/scanner")
 def scanner():
-    return render_template('scanner.html', title='Scanner')
+    form = ApiForm()
+    return render_template('scanner.html', title='Scanner', form=form)
 
 @app.route("/diagnostics")
 def diagnostics():
-    return render_template('diagnostics.html', title='Diagnostics')
+    form = ApiForm()
+    return render_template('diagnostics.html', title='Diagnostics', form=form)
 
 @app.route("/password_cracker")
 def password_cracker():
-    return render_template('password_cracker.html', title='Password Cracker')
+    form = ApiForm()
+    return render_template('password_cracker.html', title='Password Cracker', form=form)
 
 @app.route("/social_engineering")
 def social_engineering():
-    return render_template('social_engineering.html', title='Social Engineering')
+    form = ApiForm()
+    return render_template('social_engineering.html', title='Social Engineering', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
