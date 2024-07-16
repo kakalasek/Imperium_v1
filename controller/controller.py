88c5168dcc529f0bfdf69ffeff9aa3df2aa7ff87
Kakalasek <honziccekkj@seznam.cz>
2024-07-16 13:00:47 +0200

    endpoint set persistence

--- controller/controller.py
+++ controller/controller.py
@@ -5,6 +5,8 @@ import requests
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "73eeac3fa1a0ce48f381ca1e6d71f077"
 
+endpoints = [False, False, False, False]
+
 @app.route("/")
 @app.route("/home")
 def home():
@@ -16,11 +18,11 @@ def scanner():
     if request.method == 'POST' and form.validate():
         try:
             data = requests.get(form.endpoint.data).json() 
-            if data:
-                return data
+            endpoints[0] = True
+            return redirect(url_for("scanner"))
         except:
             return redirect(url_for("scanner"))
-    return render_template('scanner.html', title='Scanner', form=form)
+    return render_template('scanner.html', title='Scanner', form=form, endpoint_set=endpoints[0])
 
 @app.route("/diagnostics", methods=['GET', 'POST'])
 def diagnostics():
@@ -28,11 +30,11 @@ def diagnostics():
     if request.method == 'POST' and form.validate():
         try:
             data = requests.get(form.endpoint.data).json() 
-            if data:
-                return data
+            endpoints[1] = True
+            return redirect(url_for("diagnostics"))
         except:
             return redirect(url_for("diagnostics"))
-    return render_template('diagnostics.html', title='Diagnostics', form=form)
+    return render_template('diagnostics.html', title='Diagnostics', form=form, endpoint_set=endpoints[1])
 
 @app.route("/password_cracker", methods=['GET', 'POST'])
 def password_cracker():
@@ -40,23 +42,23 @@ def password_cracker():
     if request.method == 'POST' and form.validate():
         try:
             data = requests.get(form.endpoint.data).json() 
-            if data:
-                return data
+            endpoints[2] = True
+            return redirect(url_for("password_cracker"))
         except:
             return redirect(url_for("password_cracker"))
-    return render_template('password_cracker.html', title='Password Cracker', form=form)
+    return render_template('password_cracker.html', title='Password Cracker', form=form, endpoint_set=endpoints[2])
 
 @app.route("/social_engineering", methods=['GET', 'POST'])
 def social_engineering():
     form = ApiForm()
     if request.method == 'POST' and form.validate():
         try:
-            data = requests.get(form.endpoint.data).json() 
-            if data:
-                return data
+            data = requests.get(form.endpoint.data).json()
+            endpoints[3] = True 
+            return redirect(url_for("social_engineering"))
         except:
             return redirect(url_for("social_engineering"))
-    return render_template('social_engineering.html', title='Social Engineering', form=form)
+    return render_template('social_engineering.html', title='Social Engineering', form=form, endpoint_set=endpoints[3])
 
 if __name__ == '__main__':
     app.run(debug=True)
